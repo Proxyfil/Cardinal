70e6e902f8fddb133c50786ab1b16914c373970e
Proxyfil <62789705+Proxyfil@users.noreply.github.com>
2021-04-24 11:27:22 +0200

    Update 0.5 Package 1

--- script/scripts.js
+++ script/scripts.js
@@ -29,46 +29,53 @@ function twitchmenu(){
                 document.querySelector(".twitchmenu").classList.toggle("showmenu");
         }
 }
+function grafanamenu(){
+        try{
+                if(document.querySelector(".showmenu")!=null){
+                        document.querySelector(".showmenu").classList.toggle("showmenu");
+                }
+        }
+        finally{
+                document.querySelector(".grafanamenu").classList.toggle("showmenu");
+        }
+}
+function createWidgetTwitterTimeLine() {
+        var el = document.createElement("div"); 
+        el.innerHTML = '<a onclick="remove(this);"><img src="../content/cross.svg" width="20vh" style="float: right;"></a><input type="text" name="profil" placeholder="Profil Twitter" onkeyup="$value = value"><input onclick="updateWidgetTwitterTimeLine($value, this);" type="button" value="submit"><br>'
+        el.classList.add("brick");
+        el.classList.add("twitter");
+        el.classList.add("dragging");
+        document.getElementById("gridly").appendChild(el);
+}
 
-dragElement(document.getElementById("is-widget-movable"));
+function updateWidgetTwitterTimeLine(value, el) {
+        $profil = value
+        el.parentNode.innerHTML = ('<a onclick="remove(this);"><img src="../content/cross.svg" width="20vh" style="float: right;"></a><input type="text" name="profil" placeholder="Profil Twitter" onkeyup="$value = value"><input onclick="updateWidgetTwitterTimeLine($value, this);" type="button" value="submit"><br><a width="14vw" height="80vh" class="twitter-timeline" href="https://twitter.com/'+$profil+'">Tweets by '+$profil+'</a>');
+        setTimeout(250);
+        var script = document.createElement("script");
+                script.classList.add("script-twitter");  
+                script.type = "text/javascript";  
+                script.src = "https://platform.twitter.com/widgets.js";  
+        document.body.appendChild(script); 
+}
+function remove(el) {
+        el.parentNode.remove();
+        
+}
+function createWidgetVenastyStatsCG() {
+        var el = document.createElement("div");
+        el.innerHTML = '<a onclick="remove(this);"><img src="../content/cross.svg" width="20vh" style="float: right;"></a><br><iframe src="https://overlay.gdoc.fr/bundles/venasty/graphics/" width="500vh" height="120vh" frameborder="0"></iframe>'
+        el.classList.add("brick");
+        el.classList.add("grafana");
+        el.classList.add("dragging");
+        document.getElementById("gridly").appendChild(el);
+}
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+function createWidgetVenastyStatsStatus() {
+        var el = document.createElement("div");
+        el.innerHTML = '<a onclick="remove(this);"><img src="../content/cross.svg" width="20vh" style="float: right;"></a><br><iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTe6qeqMqKidmWyHWgH4opaVn3do2QtCJHsSrRWkWkQemnqqzdka7wawR9clm4RwYwyq5ULSLQSzCSz/pubhtml?gid=926335710&amp;single=true&amp;widget=true&amp;headers=false" width="430vh" height="250vh"></iframe>'
+        el.classList.add("brick");
+        el.classList.add("grafana");
+        el.classList.add("dragging");
+        document.getElementById("gridly").appendChild(el);
 }
